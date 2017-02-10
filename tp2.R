0d9cee3a0d5d43c5fbbf6fbecd7c48e3e68c06ac
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2017-02-10 09:16:37 -0300

    Pequenios refactors de funciones

--- tp2.R
+++ tp2.R
@@ -15,13 +15,15 @@ get_unidades_estrato <- function(n){
 estratos <- lapply(c(1,2,3), get_unidades_estrato)
 
 # Genero las PIK por estrato, dados los tamanios muestrales propuestos
-crear_piks <- function(estrato, n_muestral){
+adjuntar_piks <- function(estrato, n_muestral){
   estrato$PIK <- inclusionprobabilities(estrato$Tviv, n_muestral)
-  estrato
+  return(estrato)
 }
 
-for (i in 1:3) {
-  estratos[[i]] <- crear_piks(estratos[[i]], n_muestral_estratos[i])
+ids_estratos = as.numeric(levels(radiosTP2$Estrato))
+
+for (i in ids_estratos) {
+  estratos[[i]] <- adjuntar_piks(estratos[[i]], n_muestral_estratos[i])
 }
 
 # Compruebo que ninguna PIK sea mayor a 1:
@@ -32,7 +34,8 @@ for (i in 1:3) {
 
 # Tomo una muestra por estrato y las compongo en una `muestra_completa`
 get_muestra <- function(estrato) {
-  estrato[UPsampford(estrato$PIK) == 1,]
+  vector_inclusion <- UPsampford(estrato$PIK)
+  return(estrato[as.logical(vector_inclusion),])
 }
 
 muestras <- lapply(estratos, get_muestra)
