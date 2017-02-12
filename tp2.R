ce44aa368751d2e3d7a373656777fc712361f0e1
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2017-02-12 18:39:11 -0300

    Agrego calculo de pik segunda etapa a cada estrato

--- tp2.R
+++ tp2.R
@@ -14,7 +14,8 @@ tomar_unidades_estrato <- function(n){
 generar_estratos <- function(UP) {
 
   ids_estratos = as.numeric(levels(UP$Estrato))
-  n_muestral_estratos <- c(6, 9, 3)
+  n_UP_por_estrato <- c(6, 9, 3)
+  n_US_por_radio <- c(90, 50, 80)
   estratos <- list()
   
   for (i in ids_estratos) {
@@ -22,8 +23,8 @@ generar_estratos <- function(UP) {
     # Genero una lista con constantes fundamentales
     estratos[[i]] = list(
       id = i,
-      n = n_muestral_estratos[i]
-    )
+      nUP = n_UP_por_estrato[i],
+      nUS = n_US_por_radio[i])
   }
   
   return(estratos)
@@ -35,17 +36,20 @@ procesar_estrato <- function(estrato) {
   estrato$UP <- tomar_unidades_estrato(estrato$id)
 
   # Calculo las \Pi_(i|h)^UP
-  estrato$UP$PIK <- inclusionprobabilities(estrato$UP$Tviv, estrato$n)
+  estrato$UP$PIK_UP <- inclusionprobabilities(estrato$UP$Tviv, estrato$nUP)
 
   # Calculo la matriz de \Pi_(ij|h)^UP
-  estrato$PIKL <- UPsampfordpi2(estrato$UP$PIK)
-
+  estrato$PIKL_UP <- UPsampfordpi2(estrato$UP$PIK_UP)
+  
+  # El muestreo de segunda etapa es MSA, asi que las  
+  estrato$UP$PIK_US <- estrato$UP$PIK_UP * estrato$nUS / estrato$UP$Tviv
+  # estrato$PIKL_US <- 
   return(estrato)
 }
 
 generar_muestra <- function(estrato) {
   # Genero un vector de inclusion 'I' al azar
-  estrato$UP$I <- UPsampford(estrato$UP$PIK)
+  estrato$UP$I <- UPsampford(estrato$UP$PIK_UP)
   
   # Separo la muestra 's' que le corresponde
   estrato$s <- estrato$UP[as.logical(estrato$UP$I),]
@@ -57,8 +61,10 @@ generar_muestra <- function(estrato) {
 chequear_consistencia <- function(estratos) {
   for (estrato in estratos) {
     print(sprintf("# Estrato %s", estrato$id))
-    print(sprintf("-- Todas las PIK menores a 1: %s", any(estrato$UP$PIK < 1)))
-    print(sprintf("-- La muestra 's' tiene tamanio 'n': %s", nrow(estrato$s) == estrato$n))
+    print(sprintf("-- Todas las PIK de primera etapa menores a 1: %s",
+                  any(estrato$UP$PIK_UP < 1)))
+    print(sprintf("-- La muestra 's' tiene tamanio 'n': %s",
+                  nrow(estrato$s) == estrato$nUP))
   }
 }
 # Compruebo que ninguna PIK sea mayor a 1:
